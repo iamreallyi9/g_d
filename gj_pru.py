80a1bef94804a42dbf20b4d032963842395d382f
gj <gujian19971006@163.com>
2020-11-21 16:06:14 +0800

    ppp

--- gj_pru.py
+++ gj_pru.py
@@ -1,4 +1,5 @@
 from monodepth.mannequin_challenge.models import hourglass
+from monodepth import mannequin_challenge_model as mcm
 from torch.utils.data import DataLoader
 from loaders.video_dataset import VideoDataset, VideoFrameDataset
 import torch
@@ -7,17 +8,19 @@ import os
 from monodepth.depth_model_registry import get_depth_model
 from torchsummaryX import summary
 
+
 def get_dep():
     color_fmt = 'results/ayush/color_down/frame_{:06d}.raw'
     depth_dir = 'esults/ayush/depth_mc/depth'
 
     model = get_depth_model("mc")
     nmodel = model()
-    #new_model = hourglass.HourglassModel(3)
     print(nmodel)
+    nmodel = mcm.MannequinChallengeModel()
+    print(nmodel)
+    # new_model = hourglass.HourglassModel(3)
 
-    frames =[ i for i in range(92)]
-
+    frames = [i for i in range(92)]
 
     dataset = VideoFrameDataset(color_fmt, frames)
     data_loader = DataLoader(
@@ -28,7 +31,7 @@ def get_dep():
     torch.backends.cudnn.benchmark = True
     nmodel.eval()
 
-    #os.makedirs(depth_dir, exist_ok=True)
+    # os.makedirs(depth_dir, exist_ok=True)
     for data in data_loader:
         data = to_device(data)
         stacked_images, metadata = data
@@ -39,9 +42,9 @@ def get_dep():
         inv_depth = 1.0 / depth
     print(inv_depth)
     # print ("83")
-    iut = torch.randn(1,3,384,224)
-    summary(nmodel.model.netG, iut)
-    summary(nmodel,stacked_images)
+    iut = torch.randn(1, 3, 384, 224)
+    # summary(nmodel.model.netG, iut)
+
 
 if __name__ == '__main__':
     get_dep()
