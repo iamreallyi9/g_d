f56554ae2403e98011781476dbce51131fea0af1
gj <gujian19971006@163.com>
2020-11-26 14:16:19 +0800

    opop

---
+++ small-model.py
@@ -0,0 +1,133 @@
+
+import torch.nn as nn
+import torch.nn.functional as F
+
+class BasicBlock(nn.Module):
+    expansion = 1
+
+    def __init__(self, in_planes, planes, stride=1):
+        super(BasicBlock, self).__init__()
+        self.conv1 = nn.Conv2d(in_planes, planes, kernel_size=3, stride=stride, padding=1, bias=False)
+        self.bn1 = nn.BatchNorm2d(planes)
+        self.conv2 = nn.Conv2d(planes, planes, kernel_size=3, stride=1, padding=1, bias=False)
+        self.bn2 = nn.BatchNorm2d(planes)
+
+        self.shortcut = nn.Sequential()
+        if stride != 1 or in_planes != self.expansion*planes:
+            self.shortcut = nn.Sequential(
+                nn.Conv2d(in_planes, self.expansion*planes, kernel_size=1, stride=stride, bias=False),
+                nn.BatchNorm2d(self.expansion*planes)
+            )
+
+    def forward(self, x):
+        out = F.relu(self.bn1(self.conv1(x)))
+        out = self.bn2(self.conv2(out))
+        out += self.shortcut(x)
+        out = F.relu(out)
+        return out
+
+class Bottleneck(nn.Module):
+    expansion = 4
+
+    def __init__(self, in_planes, planes, stride=1):
+        super(Bottleneck, self).__init__()
+        self.conv1 = nn.Conv2d(in_planes, planes, kernel_size=1, bias=False)
+        self.bn1 = nn.BatchNorm2d(planes)
+        self.conv2 = nn.Conv2d(planes, planes, kernel_size=3, stride=stride, padding=1, bias=False)
+        self.bn2 = nn.BatchNorm2d(planes)
+        self.conv3 = nn.Conv2d(planes, self.expansion*planes, kernel_size=1, bias=False)
+        self.bn3 = nn.BatchNorm2d(self.expansion*planes)
+
+        self.shortcut = nn.Sequential()
+        if stride != 1 or in_planes != self.expansion*planes:
+            self.shortcut = nn.Sequential(
+                nn.Conv2d(in_planes, self.expansion*planes, kernel_size=1, stride=stride, bias=False),
+                nn.BatchNorm2d(self.expansion*planes)
+            )
+
+    def forward(self, x):
+        out = F.relu(self.bn1(self.conv1(x)))
+        out = F.relu(self.bn2(self.conv2(out)))
+        out = self.bn3(self.conv3(out))
+        out += self.shortcut(x)
+        out = F.relu(out)
+        return out
+
+class ResNet(nn.Module):
+    def __init__(self, block, num_blocks, num_classes=10):
+        super(ResNet, self).__init__()
+        self.in_planes = 32
+
+        self.conv1 = nn.Conv2d(3, 32, kernel_size=3, stride=1, padding=1, bias=False)
+        self.bn1 = nn.BatchNorm2d(32)
+        self.layer1 = self._make_layer(block, 32, num_blocks[0], stride=1)
+        self.layer2 = self._make_layer(block, 64, num_blocks[1], stride=2)
+        self.layer3 = self._make_layer(block, 128, num_blocks[2], stride=2)
+        self.layer4 = self._make_layer(block, 256, num_blocks[3], stride=2)
+        self.linear = nn.Linear(256*block.expansion, num_classes)
+
+    def _make_layer(self, block, planes, num_blocks, stride):
+        strides = [stride] + [1]*(num_blocks-1)
+        layers = []
+        for stride in strides:
+            layers.append(block(self.in_planes, planes, stride))
+            self.in_planes = planes * block.expansion
+        return nn.Sequential(*layers)
+
+    def forward(self, x):
+        out = F.relu(self.bn1(self.conv1(x)))
+        out = self.layer1(out)
+        out = self.layer2(out)
+        out = self.layer3(out)
+        out = self.layer4(out)
+        out = F.avg_pool2d(out, 4)
+        out = F.dropout(out, 1)
+        out = out.view(out.size(0), -1)
+        out = self.linear(out)
+        return out
+
+
+class _ConvLayer(nn.Sequential):
+    def __init__(self, num_input_features, num_output_features, drop_rate):
+        super(_ConvLayer, self).__init__()
+
+        self.add_module('conv', nn.Conv2d(num_input_features, num_output_features,
+                                          kernel_size=3, stride=1, padding=1, bias=False)),
+        self.add_module('relu', nn.ReLU(inplace=True)),
+        self.add_module('norm', nn.BatchNorm2d(num_output_features)),
+
+        self.drop_rate = drop_rate
+
+    def forward(self, x):
+        x = super(_ConvLayer, self).forward(x)
+        if self.drop_rate > 0:
+            x = F.dropout(x, p=self.drop_rate, training=self.training)
+        return x
+
+class CNN(nn.Module):
+    def __init__(self):
+        super(CNN, self).__init__()
+
+        self.features = nn.Sequential()
+        self.features.add_module('convlayer1', _ConvLayer(3, 32, 0.0))
+        self.features.add_module('maxpool', nn.MaxPool2d(2, 2))
+        self.features.add_module('convlayer3', _ConvLayer(32, 64, 0.0))
+        self.features.add_module('avgpool', nn.AvgPool2d(2, 2))
+        self.features.add_module('convlayer5', _ConvLayer(64, 128, 0.0))
+
+        self.classifier = nn.Linear(128, 10)
+
+        for m in self.modules():
+            if isinstance(m, nn.Conv2d):
+                nn.init.kaiming_normal_(m.weight)
+            elif isinstance(m, nn.BatchNorm2d):
+                nn.init.constant_(m.weight, 1)
+                nn.init.constant_(m.bias, 0)
+            elif isinstance(m, nn.Linear):
+                nn.init.constant_(m.bias, 0)
+
+    def forward(self, x):
+        features = self.features(x)
+        out = F.avg_pool2d(features, kernel_size=8, stride=1).view(features.size(0), -1)
+        out = self.classifier(out)
+        return out
--- test_once.py
+++ test_once.py
@@ -1,9 +1,10 @@
-
+import torch.nn as nn
+import torch.nn.functional as F
 import torch
 from torch.utils.data import DataLoader
 from monodepth.mannequin_challenge.models import hourglass
 from torch.utils.tensorboard import SummaryWriter
-
+import torch.optim as optim
 import optimizer
 from loaders.video_dataset import VideoDataset, VideoFrameDataset
 from monodepth import mannequin_challenge_model as mcm
@@ -11,11 +12,36 @@ from utils.torch_helpers import to_device
 from torchsummaryX import summary
 import torch.autograd as autograd
 import numpy as np
+from small_model import CNN
+import small_model
+def load_data():
+    color_fmt = 'results/ayush/color_down/frame_{:06d}.raw'
+    frames = [i for i in range(92)]
+    dataset = VideoFrameDataset(color_fmt, frames)
+    data_loader = DataLoader(
+        dataset, batch_size=1, shuffle=False, num_workers=4
+    )
+    return data_loader
+
+def load_t_net():
+    new_model = hourglass.HourglassModel(3)
+    new_model = torch.nn.DataParallel(new_model)
+    model_file = "results/ayush/R_hierarchical2_mc/B0.1_R1.0_PL1-0_LR0.0004_BS4_Oadam/checkpoints/0020.pth"
+    model_parameters = torch.load(model_file)
+    new_model.load_state_dict(model_parameters)
+    return new_model
+
+def test():
+
+    net = small_model.MobileNetV2()
+    x = torch.randn(2,3,32,32)
+    y = net(x)
+    summary(net,x)
+    print(y.size())
 
 def test_model():
     color_fmt = 'results/ayush/color_down/frame_{:06d}.raw'
     frames = [i for i in range(92)]
-
     dataset = VideoFrameDataset(color_fmt, frames)
     data_loader = DataLoader(
         dataset, batch_size=1, shuffle=False, num_workers=4
@@ -58,5 +84,67 @@ def test_model():
         inv_depth = 1.0 / depth
     print(inv_depth,np.shape(inv_depth))
 
+def compare():
+    # 数据集
+    data_loader =load_data()
+    #teacher——net
+    net_t = load_t_net()
+    #student——net
+    net_s = CNN()
+
+    criterion = nn.CrossEntropyLoss()
+    criterion2 = nn.KLDivLoss()
+    optimizer = optim.Adam(net_s.parameters(), lr=0.001)
+
+    device = torch.device("cuda:0" if torch.cuda.is_available() else "cpu")
+    net_s.to(device)
+    net_t.to(device)
+
+    import time
+    for epoch in range(100):
+        time_start = time.time()
+        running_loss = 0.
+        batch_size = 4
+
+        alpha = 0.95
+
+        for i, data in enumerate( data_loader):
+            inputs, labels = data
+            images = autograd.Variable(inputs.cuda(), requires_grad=False)
+            # Reshape ...CHW -> XCHW
+            shape = images.shape
+
+            C, H, W = shape[-3:]
+            images = images.reshape(-1, C, H, W)
+
+            soft_target = net_t(images)
+
+            optimizer.zero_grad()
+
+            outputs = net_s(images)
+
+            loss1 = criterion(outputs, labels)
+
+            T = 2
+            outputs_S = F.log_softmax(outputs / T, dim=1)
+            outputs_T = F.softmax(soft_target / T, dim=1)
+
+            loss2 = criterion2(outputs_S, outputs_T) * T * T
+
+            loss = loss1 * (1 - alpha) + loss2 * alpha
+
+            loss.backward()
+            optimizer.step()
+
+            print('[%d, %5d] loss: %.4f loss1: %.4f loss2: %.4f' % (
+            epoch + 1, (i + 1) * batch_size, loss.item(), loss1.item(), loss2.item()))
+
+        torch.save(net_s, '/gj_TS/student.pkl')
+        time_end = time.time()
+        print('Time cost:', time_end - time_start, "s")
+
+    print('Finished Training')
+
 if __name__ == '__main__':
-    test_model()
+    #test_model()
+    test()
