dd10ff73e7a10f4b72cce5db0912034ec07bbc00
gj <gujian19971006@163.com>
2020-12-05 15:59:49 +0800

    djjfjf

--- prune.py
+++ prune.py
@@ -59,7 +59,7 @@ def load_t_net(file = False):
 
 def test_big():
     model = load_t_net()
-    print(list(model.module.seq[3].named_parameters()))
+    print(list(model.module.seq[3].list[1][0].named_parameters()))
     device = torch.device("cuda" if torch.cuda.is_available() else "cpu")
     print("===========================")
     x=torch.randn(1,3,384,224).to(device)
@@ -68,7 +68,14 @@ def test_big():
     parameters_to_prune = (
         (model.module.seq[3].list[0][3].list[0][3].list[0][1].convs[2][3], 'weight'),
         (model.module.seq[3].list[0][3].list[0][3].list[0][1].convs[3][0], 'weight'),
-        (model.module.seq[3].list[0][3].list[0][3].list[0][1].convs[1][0], 'weight'),
+        (model.module.seq[3].list[0][1], 'weight'),
+        (model.module.seq[3].list[0][2], 'weight'),
+        (model.module.seq[3].list[0][2], 'weight'),
+        (model.module.seq[3].list[0][3], 'weight'),
+        (model.module.seq[3].list[1][0], 'weight'),
+        (model.module.seq[3].list[0][4], 'weight'),
+        (model.module.seq[3].list[0][3].list[1][1], 'weight'),
+        (model.module.seq[3].list[0][3].list[1][0], 'weight'),
         (model.module.seq[0].conv,'weight')
     )
     prune.global_unstructured(
