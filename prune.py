54bacc343a4272fa27e48cefa0136f88c047396d
gj <gujian19971006@163.com>
2020-12-05 11:59:03 +0800

    kdkdk

--- prune.py
+++ prune.py
@@ -7,52 +7,54 @@ from utils.torch_helpers import to_device
 import os
 from monodepth.depth_model_registry import get_depth_model
 from torchsummaryX import summary
-import torch.autograd as autograd
+
 import torch
 from torch import nn
 import torch.nn.utils.prune as prune
 import torch.nn.functional as F
 
-def pru_test():
+def new_prune():
+    device = torch.device("cuda" if torch.cuda.is_available() else "cpu")
 
-    new_model = hourglass.HourglassModel(3)
-    model_file = "checkpoints/test_mc.pth"
-    model_parameters = torch.load(model_file)
-    new_model.load_state_dict(model_parameters)
+    class LeNet(nn.Module):
+        def __init__(self):
+            super(LeNet, self).__init__()
+            # 1 input image channel, 6 output channels, 3x3 square conv kernel
+            self.conv1 = nn.Conv2d(1, 6, 3)
+            self.conv2 = nn.Conv2d(6, 16, 3)
+            self.fc1 = nn.Linear(16 * 5 * 5, 120)  # 5x5 image dimension
+            self.fc2 = nn.Linear(120, 84)
+            self.fc3 = nn.Linear(84, 10)
 
-    #new_model = torch.nn.DataParallel(new_model)
+        def forward(self, x):
+            x = F.max_pool2d(F.relu(self.conv1(x)), (2, 2))
+            x = F.max_pool2d(F.relu(self.conv2(x)), 2)
+            x = x.view(-1, int(x.nelement() / x.shape[0]))
+            x = F.relu(self.fc1(x))
+            x = F.relu(self.fc2(x))
+            x = self.fc3(x)
+            return x
 
-    torch.backends.cudnn.enabled = True
-    torch.backends.cudnn.benchmark = True
-    new_model.eval()
-
-    iut = torch.randn(1, 3, 384, 224)
-    summary(new_model, iut)
-    #self.seq//uncertainty_layer//pred_layer
-    module = new_model.pred_layer
-    print(list(module.named_parameters()))
-    print(list(module.named_buffers()))
-
-    prune.random_unstructured(module,name = "weight", amount = 0.3)
-    #prune.ln_structured(module, name="weight", amount=0.5, n=2, dim=0)
-    # parameters_to_prune = (
-    #     (new_model.pred_layer, 'weight'),
-    #     (new_model.uncertainty_layer, 'weight')
-    # )
-    # prune.global_unstructured(
-    #     parameters_to_prune,
-    #     pruning_method=prune.L1Unstructured,
-    #     amount=0.2,
-    # )
-    prune.remove(module, 'weight')
-    print("==========")
-
-    print(list(module.named_parameters()))
-    print(list(module.named_buffers()))
-    iut = torch.randn(1, 3, 384, 224)
-    summary(new_model, iut)
+    model = LeNet().to(device=device)
+    x = torch.randn(1,1,28,28)
+    summary(model,x)
 
 
+    parameters_to_prune = (
+        (model.conv1, 'weight'),
+        (model.conv2, 'weight'),
+        (model.fc1, 'weight'),
+        (model.fc2, 'weight'),
+        (model.fc3, 'weight'),
+    )
+
+    prune.global_unstructured(
+        parameters_to_prune,
+        pruning_method=prune.L1Unstructured,
+        amount=0.2,
+    )
+    x = torch.randn(1, 1, 28, 28)
+    summary(model, x)
 
 if __name__ == '__main__':
-    pru_test()
+    new_prune()
